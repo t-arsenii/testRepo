b5ef63a3aad23203b4e1bb040bc80e7573e0dac0
Arsenii <qweasd@gnail.com>
2023-10-05 14:37:21 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,9 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
+        name();
+    }
+    public static void name(){
+        System.out.println("Hello Arsenii!");
     }
 }
